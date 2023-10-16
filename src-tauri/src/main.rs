0947c3b3565e9e7d76064055ae9e7dde908ce467
kkcaZ <z_jagger@hotmail.co.uk>
2023-10-16 18:38:42 +0100

    feat: add socket sending

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,8 +1,25 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use std::io::prelude::*;
+use std::net::TcpStream;
+
 fn main() {
   tauri::Builder::default()
+    .invoke_handler(tauri::generate_handler![send_tcp_message])
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
+}
+
+#[tauri::command]
+fn send_tcp_message(message: String) -> String {
+    println!("Sending Message: {}", message);
+
+    let mut stream = TcpStream::connect("localhost:8080").unwrap();
+    stream.write(&message.as_bytes()).unwrap();
+
+    let mut buffer = [0; 128];
+    stream.read(&mut buffer).unwrap();
+    let response = String::from_utf8_lossy(&buffer[..]);
+    return response.to_string()
 }
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,18 @@
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<button on:click={() => sayHello()} >Say Hello</button>
+<p>{text}</p>
+
+<script lang="ts ">
+  import { invoke } from "@tauri-apps/api/tauri";
+
+  let text = "Test Text!"
+
+  function sayHello() {
+    console.log("Attempting to say hello!");
+    invoke('send_tcp_message', { message: 'Hello!' }).then(r => {
+      text = r;
+    })
+  }
+</script>
