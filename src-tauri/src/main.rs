a299a6cb28ba5b24056a2fb25cdbab1532a74e01
kkcaZ <z_jagger@hotmail.co.uk>
2023-10-19 22:26:17 +0100

    feat: show dev tools in debug mode

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,11 +1,20 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use tauri::Manager;
 use std::io::prelude::*;
 use std::net::TcpStream;
 
 fn main() {
   tauri::Builder::default()
+  .setup(|app| {
+  #[cfg(debug_assertions)]
+  {
+    let window = app.get_window("main").unwrap();
+    window.open_devtools();
+  }
+  Ok(())
+  })
     .invoke_handler(tauri::generate_handler![send_tcp_message])
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
