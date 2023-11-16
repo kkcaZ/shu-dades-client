a58a250b029521698b7b6f49d1a21afa56837851
kkcaZ <z_jagger@hotmail.co.uk>
2023-11-16 17:46:48 +0000

    refactor: maintain single tcp connection

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -6,17 +6,32 @@ use tauri::Manager;
 
 use std::io::prelude::*;
 use std::net::TcpStream;
+use std::sync::{Mutex, Once};
+
+static mut GLOBAL_TCP_STREAM: Option<Mutex<TcpStream>> = None;
+static INIT: Once = Once::new();
+
+fn initialize_global_tcp_stream() {
+    INIT.call_once(|| {
+        let tcp_stream = TcpStream::connect("localhost:8080").expect("Failed to connect to address");
+        unsafe {
+            GLOBAL_TCP_STREAM = Some(Mutex::new(tcp_stream));
+        }
+    });
+}
 
 fn main() {
-  tauri::Builder::default()
-  .setup(|_app| {
-  #[cfg(debug_assertions)]
-  {
+    initialize_global_tcp_stream();
+
+    tauri::Builder::default()
+    .setup(|_app| {
+    #[cfg(debug_assertions)]
+    {
     let window = _app.get_window("main").unwrap();
     window.open_devtools();
-  }
-  Ok(())
-  })
+    }
+    Ok(())
+    })
     .invoke_handler(tauri::generate_handler![send_tcp_message])
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
@@ -24,9 +39,10 @@ fn main() {
 
 #[tauri::command]
 fn send_tcp_message(message: String) -> String {
-    println!("Sending Message: {}", message);
+    let mutex = unsafe { GLOBAL_TCP_STREAM.as_ref().expect("TcpStream not initialized") };
+    let mut stream = mutex.lock().expect("Failed to lock TcpStream");
 
-    let mut stream = TcpStream::connect("localhost:8080").unwrap();
+    println!("Sending Message: {}", message);
     stream.write(&message.as_bytes()).unwrap();
 
     let mut buffer = [0; 4096];
